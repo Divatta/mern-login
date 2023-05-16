91261604d7f9f7661713f2c206d07d94ae85dfa3
Divatta <divattadil@gmail.com>
2023-05-16 11:01:57 +0530

    first commit

--- server.js
+++ server.js
@@ -31,6 +31,21 @@ app.all('*', (req,res,next) =>{
     next()
 })
 
+// Your code
+if (process.env.NODE_ENV === "production") {
+    const path = require("path");
+    app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+    app.get("*", (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'),function (err) {
+            if(err) {
+                res.status(500).send(err)
+            }
+        });
+    })
+}
+// Your code
+
+
 app.listen(PORT, async () => {
     await connectDb(process.env.MONGO_URL)
     console.log(`server is started @ http://localhost:${PORT}`)
